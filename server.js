2aca6c12125340174fbf867374ec8f7a22b5fc44
Osama-Same <osama.moh.salem@gmail.com>
2022-05-29 02:28:29 +0300

    my first commit

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
-const express = require('express');
-const cors = require("cors")
-const mysql = require("./Connection/connection");
-const app = express()
+const express = require("express");
+const cors = require("cors");
+require("./Connection/connection");
+require("dotenv").config();
+const app = express();
+
+// cors
+app.use(cors());
+
+// express json
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    const sql = `select * from users`;
+    mysql.query(sql, (err, result) => {
+      if (err) {
+        res.send({ err: "error" });
+      }
+      if (result) {
+        res.json({ result: result });
+      }
+    });
+  });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
