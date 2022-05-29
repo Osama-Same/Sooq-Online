039e89e24917631db61f3d75b31c0526f7b0e3b2
Osama-Same <osama.moh.salem@gmail.com>
2022-05-29 04:09:49 +0300

    my first commit

--- server.js
+++ server.js
@@ -5,17 +5,13 @@ const router = require("./Router/Router");
 const path = require("path");
 require("dotenv").config();
 const app = express();
-
 // cors
 app.use(cors());
-
 // express json
 app.use(express.json());
+app.use(express.Router());
 
-// Routre
-app.use(router);
 
-app.use(express.static(path.resolve(__dirname, "Router/Router")));
 app.use(express.static(path.resolve(__dirname, "client/build")));
 
 
