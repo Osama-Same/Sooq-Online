0603b6c6c5f1662b5543f6c5961db610acae635a
Osama-Same <osama.moh.salem@gmail.com>
2022-05-29 22:30:53 +0300

    my first commit

--- server.js
+++ server.js
@@ -36,8 +36,6 @@ app.use(express.static("./Images/user"));
 
 
 app.use(express.static(path.resolve(__dirname, "client/build")));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
