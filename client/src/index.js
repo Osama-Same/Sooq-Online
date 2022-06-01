a93acfe965333dcbdf2fa8f3ad04a7392fbee4d4
Osama-Same <osama.moh.salem@gmail.com>
2022-06-01 09:13:04 +0300

    server

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 import axios from "axios";
 
 
-axios.defaults.baseURL =`http://localhost:5000/`
-//axios.defaults.baseURL =`https://sooq-online.herokuapp.com/`
+//axios.defaults.baseURL =`http://localhost:5000/`
+axios.defaults.baseURL =`https://sooq-online.herokuapp.com/`
 ReactDOM.render(
   <App />,
   document.getElementById('root')
