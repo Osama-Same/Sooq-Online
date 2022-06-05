e30824a0178656a6f56f6c065081100a45d79836
Osama-Same <osama.moh.salem@gmail.com>
2022-06-06 02:26:26 +0300

    make it better

--- server.js
+++ server.js
@@ -59,9 +59,7 @@ app.use(express.static(path.resolve(__dirname, "client/build")));
 
 const PORT = process.env.PORT || 5000;
 
-    app.listen(PORT, () => {
-      console.log(` http://localhost:${PORT}`);
-    });
+app.listen(PORT, () => console.log(`REVIEW at http://localhost:${PORT}`));
 
 
 
